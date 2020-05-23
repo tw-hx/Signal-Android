d6b0515117fa904cf84656d1ec3fa80bab8dfd6d
Angus Turnbull <github@twinhelix.com>
2020-05-24 01:07:57 +1200

    Replaced Google Mobile Services in Website build with open source code

---
+++ app/src/website/java/com/google/android/gms/common/api/Status.java
@@ -0,0 +1,10 @@
+package com.google.android.gms.common.api;
+
+public final class Status {
+  public Status(int statusCode) {
+  }
+
+  public int getStatusCode() {
+    return CommonStatusCodes.ERROR;
+  }
+}
---
+++ app/src/website/java/com/google/firebase/messaging/FirebaseMessaging.java
@@ -0,0 +1,15 @@
+package com.google.firebase.messaging;
+
+public class FirebaseMessaging {
+
+    public static FirebaseMessaging getInstance() {
+        return new FirebaseMessaging();
+    }
+
+    public void unsubscribeFromTopic(String s) {
+    }
+
+    public void subscribeToTopic(String ref) {
+    }
+
+}
