62085fb77aa382b39cbcc235705f544e491d08b9
Angus Turnbull <github@twinhelix.com>
2020-10-29 19:35:09 +1300

    Stub out Google Mobile Services and Firebase with Open Source library, new build flavor 'gms' to use it

---
+++ libfakegms/src/main/java/com/google/firebase/messaging/RemoteMessage.java
@@ -0,0 +1,21 @@
+package com.google.firebase.messaging;
+
+public class RemoteMessage {
+  public Data getData() {
+    return new Data();
+  }
+
+  public String getMessageId() {
+    return "";
+  }
+
+  public long getSentTime() {
+    return 0;
+  }
+
+  public static class Data {
+    public String get(String challenge) {
+      return null;
+    }
+  }
+}
