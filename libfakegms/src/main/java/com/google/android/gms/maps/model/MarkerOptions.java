9c0f1494587a646eedc85ff39c1575709658cc40
Angus Turnbull <github@twinhelix.com>
2020-10-29 19:35:09 +1300

    Stub out Google Mobile Services and Firebase with Open Source library, new build flavor 'gms' to use it

---
+++ libfakegms/src/main/java/com/google/android/gms/maps/model/MarkerOptions.java
@@ -0,0 +1,9 @@
+package com.google.android.gms.maps.model;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public final class MarkerOptions {
+  public MarkerOptions position(LatLng latlng) {
+    return this;
+  }
+}
