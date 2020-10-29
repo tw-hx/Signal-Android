0b297d989fb4ed175d9139837cac2d80f8e451c8
Angus Turnbull <github@twinhelix.com>
2020-10-29 19:35:09 +1300

    Stub out Google Mobile Services and Firebase with Open Source library, new build flavor 'gms' to use it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ val selectableVariants = listOf(
   "playStagingInstrumentation",
   "playStagingRelease",
   "websiteProdSpinner",
+  "websiteFossProdRelease",
+  "websiteGmsProdRelease",
   "websiteProdRelease"
 )
 
@@ -83,7 +85,7 @@ android {
   compileSdkVersion = signalCompileSdkVersion
   ndkVersion = signalNdkVersion
 
-  flavorDimensions += listOf("distribution", "environment")
+  flavorDimensions += listOf("distribution", "gms", "environment")
   useLibrary("org.apache.http.legacy")
   testBuildType = "instrumentation"
 
@@ -351,8 +353,10 @@ android {
     }
 
     create("website") {
+      val MAU = if (gradle.startParameter.taskRequests.toString().contains("Foss")) "false" else "true"
+
       dimension = "distribution"
-      buildConfigField("boolean", "MANAGES_APP_UPDATES", "true")
+      buildConfigField("boolean", "MANAGES_APP_UPDATES", "${MAU}")
       buildConfigField("String", "APK_UPDATE_MANIFEST_URL", "\"https://updates.signal.org/android/latest.json\"")
       buildConfigField("String", "BUILD_DISTRIBUTION_TYPE", "\"website\"")
     }
@@ -369,6 +373,17 @@ android {
       buildConfigField("boolean", "MANAGES_APP_UPDATES", "true")
       buildConfigField("String", "APK_UPDATE_MANIFEST_URL", "\"${apkUpdateManifestUrl}\"")
       buildConfigField("String", "BUILD_DISTRIBUTION_TYPE", "\"nightly\"")
+    }
+
+    create("gms") {
+      dimension = "gms"
+      isDefault = true
+      buildConfigField("boolean", "USE_OSM", "false")
+    }
+
+    create("foss") {
+      dimension = "gms"
+      buildConfigField("boolean", "USE_OSM", "true")
     }
 
     create("prod") {
@@ -527,13 +542,15 @@ dependencies {
   implementation(libs.androidx.asynclayoutinflater)
   implementation(libs.androidx.asynclayoutinflater.appcompat)
   implementation(libs.androidx.emoji2)
-  implementation(libs.firebase.messaging) {
+  "gmsImplementation"(libs.firebase.messaging) {
     exclude(group = "com.google.firebase", module = "firebase-core")
     exclude(group = "com.google.firebase", module = "firebase-analytics")
     exclude(group = "com.google.firebase", module = "firebase-measurement-connector")
   }
-  implementation(libs.google.play.services.maps)
-  implementation(libs.google.play.services.auth)
+  "gmsImplementation"(libs.google.play.services.maps)
+  "gmsImplementation"(libs.google.play.services.auth)
+  "fossImplementation"(project(":libfakegms"))
+  "fossImplementation"(libs.osmdroid)
   implementation(libs.bundles.media3)
   implementation(libs.conscrypt.android)
   implementation(libs.signal.aesgcmprovider)
@@ -570,7 +587,7 @@ dependencies {
   implementation(libs.accompanist.permissions)
   implementation(libs.kotlin.stdlib.jdk8)
   implementation(libs.kotlin.reflect)
-  implementation(libs.kotlinx.coroutines.play.services)
+  "gmsImplementation"(libs.kotlinx.coroutines.play.services)
   implementation(libs.kotlinx.coroutines.rx3)
   implementation(libs.jackson.module.kotlin)
   implementation(libs.rxjava3.rxandroid)
