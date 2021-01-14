0bf774776600170784f2632f58e8f6a88b87efc9
Angus Turnbull <github@twinhelix.com>
2021-01-14 19:40:21 +1300

    Github CI workflow to build, sign and release using Base64 encoded keystore

--- app/src/main/java/org/thoughtcrime/securesms/util/AttachmentUtil.java
+++ app/src/main/java/org/thoughtcrime/securesms/util/AttachmentUtil.java
@@ -12,7 +12,6 @@ import org.signal.core.util.logging.Log;
 import org.thoughtcrime.securesms.attachments.AttachmentId;
 import org.thoughtcrime.securesms.attachments.DatabaseAttachment;
 import org.thoughtcrime.securesms.database.NoSuchMessageException;
-import org.thoughtcrime.securesms.database.MmsDatabase;
 import org.thoughtcrime.securesms.database.SignalDatabase;
 import org.thoughtcrime.securesms.database.model.MessageRecord;
 import org.thoughtcrime.securesms.jobmanager.impl.NotInCallConstraint;
