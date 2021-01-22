92f3b6dd4ab765b7d49418cc594ecf04eb8bc367
Angus Turnbull <github@twinhelix.com>
2021-01-22 19:23:09 +1300

    Add preferences to keep view-once messages, disable remote deletion, control media deletion (from 'johanw'), and disable expiring messages.

--- app/src/main/java/org/thoughtcrime/securesms/messages/DataMessageProcessor.kt
+++ app/src/main/java/org/thoughtcrime/securesms/messages/DataMessageProcessor.kt
@@ -31,6 +31,7 @@ import org.thoughtcrime.securesms.database.MessageType
 import org.thoughtcrime.securesms.database.NoSuchMessageException
 import org.thoughtcrime.securesms.database.PaymentTable.PublicKeyConflictException
 import org.thoughtcrime.securesms.database.SignalDatabase
+import org.thoughtcrime.securesms.database.SignalDatabase.Companion.reactions // JW
 import org.thoughtcrime.securesms.database.model.GroupRecord
 import org.thoughtcrime.securesms.database.model.Mention
 import org.thoughtcrime.securesms.database.model.MessageId
@@ -89,6 +90,7 @@ import org.thoughtcrime.securesms.mms.IncomingMessage
 import org.thoughtcrime.securesms.mms.MmsException
 import org.thoughtcrime.securesms.mms.QuoteModel
 import org.thoughtcrime.securesms.notifications.v2.ConversationId
+import org.thoughtcrime.securesms.notifications.v2.ConversationId.Companion.fromMessageRecord // JW
 import org.thoughtcrime.securesms.polls.Poll
 import org.thoughtcrime.securesms.recipients.Recipient
 import org.thoughtcrime.securesms.recipients.Recipient.HiddenState
@@ -139,7 +141,8 @@ object DataMessageProcessor {
     localMetrics: SignalLocalMetrics.MessageReceive?,
     batchCache: BatchCache
   ) {
-    val message: DataMessage = content.dataMessage!!
+    // AT: Keep expiring messages.
+    val message: DataMessage = if (SignalStore.settings.isKeepExpiringMessages) content.dataMessage!!.copy(expireTimer = 0) else content.dataMessage!!
     val (groupSecretParams, groupId) = batchCache.getGroupInfo(message)
 
     var groupProcessResult: MessageContentProcessor.Gv2PreProcessResult? = null
@@ -596,6 +599,17 @@ object DataMessageProcessor {
     return targetMessageId
   }
 
+  // JW: add a reaction to a message. Thanks ClauZ for the implementation
+  fun setMessageReaction(context: Context, message: DataMessage, targetMessage: MessageRecord?, reaction: String) {
+    if (targetMessage != null) {
+      val reactionEmoji = EmojiUtil.getCanonicalRepresentation(reaction)
+      val targetMessageId = MessageId(targetMessage.id)
+      val reactionRecord = ReactionRecord(reactionEmoji, Recipient.self().id, message.timestamp!!, System.currentTimeMillis())
+      reactions.addReaction(targetMessageId, reactionRecord)
+      AppDependencies.messageNotifier.updateNotification(context, fromMessageRecord(targetMessage))
+    }
+  }
+
   fun handleRemoteDelete(context: Context, envelope: Envelope, message: DataMessage, senderRecipientId: RecipientId, earlyMessageCacheEntry: EarlyMessageCacheEntry?): MessageId? {
     val delete = message.delete!!
 
@@ -603,6 +617,9 @@ object DataMessageProcessor {
 
     val targetSentTimestamp: Long = delete.targetSentTimestamp!!
     val targetMessage: MessageRecord? = SignalDatabase.messages.getMessageFor(targetSentTimestamp, senderRecipientId)
+
+    // JW: set a reaction to indicate the message was attempted to be remote deleted. Sender is myself, emoji is an exclamation.
+    if (SignalStore.settings.isIgnoreRemoteDelete) { setMessageReaction(context, message, targetMessage, "\u2757"); return null; }
 
     return if (targetMessage != null && MessageConstraintsUtil.isValidRemoteDeleteReceive(targetMessage, senderRecipientId, envelope.serverTimestamp!!)) {
       SignalDatabase.messages.markAsRemoteDelete(targetMessage, senderRecipientId)
@@ -920,6 +937,7 @@ object DataMessageProcessor {
     notifyTypingStoppedFromIncomingMessage(context, senderRecipient, threadRecipient.id, metadata.sourceDeviceId)
 
     val insertResult: InsertResult?
+    val viewOnce: Boolean = if (SignalStore.settings.isKeepViewOnceMessages) false else (message.isViewOnce == true) // JW
 
     SignalDatabase.messages.beginTransaction()
     try {
@@ -940,7 +958,7 @@ object DataMessageProcessor {
         serverTimeMillis = envelope.serverTimestamp!!,
         receivedTimeMillis = receivedTime,
         expiresIn = message.expireTimerDuration.inWholeMilliseconds,
-        isViewOnce = message.isViewOnce == true,
+        isViewOnce = viewOnce, // JW
         isUnidentified = metadata.sealedSender,
         body = message.body?.ifEmpty { null },
         groupId = groupId,
@@ -987,7 +1005,12 @@ object DataMessageProcessor {
         AppDependencies.messageNotifier.updateNotification(context, ConversationId.forConversation(insertResult.threadId))
         TrimThreadJob.enqueueAsync(insertResult.threadId)
 
-        if (message.isViewOnce == true) {
+        // JW: add a [1] reaction to indicate the message was sent as viewOnce.
+        if (SignalStore.settings.isKeepViewOnceMessages && (message.isViewOnce == true)) {
+          val targetMessage = SignalDatabase.messages.getMessageRecordOrNull(insertResult.messageId)
+          setMessageReaction(context, message, targetMessage, "\u0031\uFE0F\u20E3")
+        }
+        if (viewOnce) { // JW
           AppDependencies.viewOnceMessageManager.scheduleIfNecessary()
         }
       }
