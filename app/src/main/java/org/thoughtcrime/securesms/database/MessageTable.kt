27a6b6f81de0fff808e113718f2f5f1bdef9be9e
Angus Turnbull <github@twinhelix.com>
2021-01-22 19:23:09 +1300

    Add preferences to keep view-once messages, disable remote deletion, control media deletion (from 'johanw'), and disable expiring messages.

--- app/src/main/java/org/thoughtcrime/securesms/database/MessageTable.kt
+++ app/src/main/java/org/thoughtcrime/securesms/database/MessageTable.kt
@@ -5775,4 +5775,15 @@ open class MessageTable(context: Context?, databaseHelper: SignalDatabase) : Dat
       }
     }
   }
+
+  // JW: Deletes only the attachment for the message, not the message itself.
+  fun deleteAttachmentsOnly(messageId: Long): Boolean {
+    val threadId = getThreadIdForMessage(messageId)
+    val attachmentTable = SignalDatabase.attachments
+    attachmentTable.deleteAttachmentsForMessage(messageId)
+    notifyConversationListeners(threadId)
+    OptimizeMessageSearchIndexJob.enqueue()
+    return true
+  }
+
 }
