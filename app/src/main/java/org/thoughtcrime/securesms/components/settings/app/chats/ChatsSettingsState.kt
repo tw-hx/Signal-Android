ccb641e323360639c17ab7c6514f9376abe67cf8
Angus Turnbull <github@twinhelix.com>
2021-01-22 19:23:09 +1300

    Add preferences to keep view-once messages, disable remote deletion, control media deletion (from 'johanw'), and disable expiring messages.

--- app/src/main/java/org/thoughtcrime/securesms/components/settings/app/chats/ChatsSettingsState.kt
+++ app/src/main/java/org/thoughtcrime/securesms/components/settings/app/chats/ChatsSettingsState.kt
@@ -10,6 +10,12 @@ data class ChatsSettingsState(
   val folderCount: Int,
   val userUnregistered: Boolean,
   val clientDeprecated: Boolean
+  // JW: added extra preferences
+  ,
+  val keepViewOnceMessages: Boolean,
+  val keepExpiringMessages: Boolean,
+  val ignoreRemoteDelete: Boolean,
+  val deleteMediaOnly: Boolean
 ) {
   fun isRegisteredAndUpToDate(): Boolean {
     return !userUnregistered && !clientDeprecated
