b726a689bc4e51f498b5a28dcdb11589f787257f
Angus Turnbull <github@twinhelix.com>
2021-01-22 19:23:09 +1300

    Add preferences to keep view-once messages, disable remote deletion, control media deletion (from 'johanw'), and disable expiring messages.

--- app/src/main/java/org/thoughtcrime/securesms/components/settings/app/chats/ChatsSettingsViewModel.kt
+++ app/src/main/java/org/thoughtcrime/securesms/components/settings/app/chats/ChatsSettingsViewModel.kt
@@ -31,6 +31,12 @@ class ChatsSettingsViewModel @JvmOverloads constructor(
       folderCount = 0,
       userUnregistered = TextSecurePreferences.isUnauthorizedReceived(AppDependencies.application) || !SignalStore.account.isRegistered,
       clientDeprecated = SignalStore.misc.isClientDeprecated
+      // AT: added
+      ,
+      keepViewOnceMessages = SignalStore.settings.isKeepViewOnceMessages,
+      keepExpiringMessages = SignalStore.settings.isKeepExpiringMessages,
+      ignoreRemoteDelete = SignalStore.settings.isIgnoreRemoteDelete,
+      deleteMediaOnly = SignalStore.settings.isDeleteMediaOnly
     )
   )
 
@@ -86,4 +92,29 @@ class ChatsSettingsViewModel @JvmOverloads constructor(
       }
     }
   }
+
+  // AT: added
+  fun setKeepViewOnceMessages(enabled: Boolean) {
+    store.update { it.copy(keepViewOnceMessages = enabled) }
+    SignalStore.settings.isKeepViewOnceMessages = enabled
+  }
+
+  // AT: added
+  fun setKeepExpiringMessages(enabled: Boolean) {
+    store.update { it.copy(keepExpiringMessages = enabled) }
+    SignalStore.settings.isKeepExpiringMessages = enabled
+  }
+
+  // AT: added
+  fun setIgnoreRemoteDelete(enabled: Boolean) {
+    store.update { it.copy(ignoreRemoteDelete = enabled) }
+    SignalStore.settings.isIgnoreRemoteDelete = enabled
+  }
+
+  // AT: added
+  fun setDeleteMediaOnly(enabled: Boolean) {
+    store.update { it.copy(deleteMediaOnly = enabled) }
+    SignalStore.settings.isDeleteMediaOnly = enabled
+  }
+
 }
