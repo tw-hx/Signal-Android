fde74ca56c48e5beee123756525c55746965f6a0
Angus Turnbull <github@twinhelix.com>
2026-01-25 21:04:18 +1300

    Stub out Google Mobile Services and Firebase with Open Source library, new build flavor 'gms' to use it

---
+++ lib/fakegms/src/main/java/com/google/firebase/FirebaseApp.java
@@ -0,0 +1,9 @@
+package com.google.firebase;
+
+import android.content.Context;
+
+public class FirebaseApp extends Object {
+  public static FirebaseApp initializeApp(Context context) {
+    return new FirebaseApp();
+  }
+}
