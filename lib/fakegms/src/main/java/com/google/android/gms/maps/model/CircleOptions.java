47aa22afe8dfee9d6aac7875aac958abaad10ff0
Angus Turnbull <github@twinhelix.com>
2026-01-25 21:04:18 +1300

    Stub out Google Mobile Services and Firebase with Open Source library, new build flavor 'gms' to use it

---
+++ lib/fakegms/src/main/java/com/google/android/gms/maps/model/CircleOptions.java
@@ -0,0 +1,29 @@
+package com.google.android.gms.maps.model;
+
+import android.content.Context;
+
+public final class CircleOptions {
+  public CircleOptions() {
+  }
+
+  public CircleOptions center(LatLng center) {
+    return this;
+  }
+
+  public CircleOptions fillColor(int color) {
+    return this;
+  }
+
+  public CircleOptions radius(double radius) {
+    return this;
+  }
+
+  public CircleOptions strokeColor(int color) {
+    return this;
+  }
+
+  public CircleOptions strokeWidth(float width) {
+    return this;
+  }
+
+}
